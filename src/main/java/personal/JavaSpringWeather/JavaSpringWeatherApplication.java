c9ee24846f3da54d27d07aebe844a92a87582188
AuDietrich <33529775+AusDietrich@users.noreply.github.com>
2020-08-06 10:23:36 -0700

    Add files via upload

---
+++ src/main/java/personal/JavaSpringWeather/JavaSpringWeatherApplication.java
@@ -0,0 +1,13 @@
+package personal.JavaSpringWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/JavaSpringWeather/JavaSpringWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.JavaSpringWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
